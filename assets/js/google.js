f3f3229815e856faa80e4b00125778ad26af2438
peterb <prbayne@gmail.com>
2020-10-24 15:53:07 +1300

    Force anon to leave

--- assets/js/google.js
+++ assets/js/google.js
@@ -13,24 +13,34 @@ var initSigninV2 = function() {
     auth2.attachClickHandler(document.getElementById('signInButton'));
     auth2.currentUser.listen(userChanged);
     // Sign in the user if they are currently signed in.
-    $('body').removeClass('unchecked');
     if (auth2.isSignedIn.get() == true) {
-        auth2.signIn();
-        $('body').addClass('signedIn');
-    }else{
-      $('body').addClass('anon');
+      console.log("Already signed in");
+      auth2.signIn();
+      $('body').addClass('signedIn');
+      $('body').removeClass('unchecked');
+      $('body').removeClass('anon');
     }
 };
 
 var userChanged = function (user) {
     console.log('User now: ', user);
     googleUser = user;
-    googleUserName = user.getBasicProfile().getName();
-    document.getElementById('signInButton').classList.add("loggedin")
-    document.getElementById('signInLabel').innerText = user.getBasicProfile().getName();
-    document.getElementById('signInPic').src = user.getBasicProfile().getImageUrl();
-    $('body').removeClass('unchecked');
-    $('body').addClass('signedIn');
-    makeVisit(user.getId());
-    loadVisited(user.getId(), $(".toc"));
+    googleProfile = googleUser.getBasicProfile();
+    if(googleProfile){
+      googleUserName = googleProfile.getName();
+      console.log(`Signed in as ${googleUserName}`);
+      document.getElementById('signInButton').classList.add("loggedin")
+      document.getElementById('signInLabel').innerText = user.getBasicProfile().getName();
+      document.getElementById('signInPic').src = user.getBasicProfile().getImageUrl();
+      $('body').addClass('signedIn');
+      $('body').removeClass('anon');
+      $('body').removeClass('unchecked');
+      makeVisit(user.getId());
+      loadVisited(user.getId(), $(".toc"));
+    }else{
+      console.log("Not currently signed in");
+      $('body').addClass('anon');
+      $('body').removeClass('unchecked');
+      $('body').removeClass('signedIn');
+    }
 };    
